089b0b635577a824dc3f6ae5312f48ce7206c69e
Irek Romaniuk <irekromaniuk@Ireks-Mac-mini.local>
2020-01-03 15:01:53 -0500

    Variables updated

--- 2-certificates.tf
+++ 2-certificates.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault" "vault" {
-  name                = "${var.vault}"
-  resource_group_name = "${var.rg_vault}"
+  name                = "${data.azurerm_key_vault_secret.name}"
+  resource_group_name = "${data.azurerm_key_vault_secret.resource_group_name}"
 }
 
