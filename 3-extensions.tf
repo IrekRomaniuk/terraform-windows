c286e2bad7a2f76b59267b864e9244e3be8e53b8
Irek Romaniuk <irekromaniuk@Ireks-Mac-mini.local>
2020-01-06 10:57:10 -0500

    extension updated

---
+++ 3-extensions.tf
@@ -0,0 +1,20 @@
+resource "azurerm_virtual_machine_extension" "disk-encryption" {
+  name                 = "DiskEncryption"
+  location             = "${data.azurerm_resource_group.rg.location}"
+ resource_group_name = "${data.azurerm_resource_group.rg.name}"
+  virtual_machine_name = "${data.azurerm_virtual_machine.vm.name}"
+  publisher            = "Microsoft.Azure.Security"
+  type                 = "AzureDiskEncryption"
+  type_handler_version = "2.2"
+
+  settings = <<SETTINGS
+{
+  "EncryptionOperation": "EnableEncryption",
+  "KeyVaultURL": "https://${var.vault}.vault.azure.net",
+  "KeyVaultResourceId": "${data.azurerm_key_vault.vault.id}"
+  "KeyEncryptionKeyURL": "${var.vault_uri}",
+  "KeyEncryptionAlgorithm": "RSA-OAEP",
+  "VolumeType": "All"
+}
+SETTINGS
+}
